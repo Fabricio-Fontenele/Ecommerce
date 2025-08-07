ec429d4eec62ef2456bfc7efda3e4e37c0b6c524
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-08-06 22:34:15 -0300

    feat: add ProductItem and ProductList components with currency formatting
    - Implemented ProductItem component to display individual product details including image, name, description, and price.
    - Created ProductList component to render a list of ProductItem components with a title and horizontal scrolling.
    - Added formatCentsToBRL helper function to format price in cents to Brazilian Real currency format.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Header from "@/components/common/header";
+import Image from "next/image";
 
-export default function Home() {
+import Header from "@/components/common/header";
+import ProductsList from "@/components/common/productsList";
+import { db } from "@/db";
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
   return (
     <>
       <Header />
+      <div className="space-y-6 px-5">
+        <Image
+          src="/banner.svg"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="mt-5 h-auto w-full"
+        />
+        <ProductsList products={products} title="Produtos em destaque" />
+        <Image
+          src="/banner2.svg"
+          alt="Seja automatico"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="mt-5 h-auto w-full"
+        />
+      </div>
     </>
   );
 }
