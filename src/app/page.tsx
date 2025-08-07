8ab24d8a151a4c43ff2b3a90d07cc9f67c7d5b51
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-08-06 22:46:40 -0300

    refactor: improve Home component structure and update ProductList usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,64 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
+import CategorySelector from "@/components/common/categorySelector";
+import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
-import ProductsList from "@/components/common/productsList";
+import ProductList from "@/components/common/productsList";
 import { db } from "@/db";
-export default async function Home() {
+import { productTable } from "@/db/schema";
+
+const Home = async () => {
   const products = await db.query.productTable.findMany({
     with: {
       variants: true,
     },
   });
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true,
+    },
+  });
+  const categories = await db.query.categoryTable.findMany({});
+
   return (
     <>
       <Header />
-      <div className="space-y-6 px-5">
-        <Image
-          src="/banner.svg"
-          alt="Leve uma vida com estilo"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="mt-5 h-auto w-full"
-        />
-        <ProductsList products={products} title="Produtos em destaque" />
-        <Image
-          src="/banner2.svg"
-          alt="Seja automatico"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="mt-5 h-auto w-full"
-        />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner.svg"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
+
+        <div className="px-5">
+          <Image
+            src="/banner2.svg"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={newlyCreatedProducts} title="Novos produtos" />
+        <Footer />
       </div>
     </>
   );
-}
+};
+
+export default Home;
