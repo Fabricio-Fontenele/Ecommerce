085f0b019b3d759b50e84a38f4a7cede188ca68a
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-08-10 19:01:41 -0300

    feat: add revalidation for cart identification after creating shipping address

--- src/actions/createShippingAddress/index.ts
+++ src/actions/createShippingAddress/index.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 
 import { db } from "@/db";
@@ -45,6 +46,8 @@ export const createShippingAddress = async (
       cpfOrCnpj: validatedData.cpf,
     })
     .returning();
+
+  revalidatePath("/cart/identification");
 
   return {
     success: true,
