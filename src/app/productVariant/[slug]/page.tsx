62592fd25defbe7de23e3a8c441ec2cf293da8bd
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-08-10 01:46:06 -0300

    feat: integrate React Query for cart management and implement product actions

--- src/app/productVariant/[slug]/page.tsx
+++ src/app/productVariant/[slug]/page.tsx
@@ -5,12 +5,11 @@ import { notFound } from "next/navigation";
 import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import ProductList from "@/components/common/productsList";
-import { Button } from "@/components/ui/button";
 import { db } from "@/db";
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
 
-import QuantitySelector from "./components/quantitySelector";
+import ProductActions from "./components/productActions";
 import VariantSelector from "./components/variantSelector";
 
 interface ProductVariantPageProps {
@@ -77,19 +76,7 @@ const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
           </h3>
         </div>
 
-        <div className="px-5">
-          <QuantitySelector />
-        </div>
-
-        <div className="flex flex-col space-y-4 px-5">
-          {/* Botões de Ação */}
-          <Button className="rounded-full" size="lg" variant={"outline"}>
-            Adicionar à sacola
-          </Button>
-          <Button className="rounded-full" size="lg">
-            Comprar agora
-          </Button>
-        </div>
+        <ProductActions productVariantId={productVariant.id} />
 
         <div className="px-5">
           <p className="text-sm">{productVariant.product.description}</p>
--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
 import { ShoppingBasketIcon } from "lucide-react";
+import Image from "next/image";
+
+import { getCart } from "@/actions/getCart";
 
 import { Button } from "../ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
 
-const Cart = () => {
+export const Cart = () => {
+  const { data: cart, isLoading: cartIsLoading } = useQuery({
+    queryKey: ["cart"],
+    queryFn: async () => getCart(),
+  });
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -13,7 +27,32 @@ const Cart = () => {
           <ShoppingBasketIcon />
         </Button>
       </SheetTrigger>
-      <SheetContent></SheetContent>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Carrinho</SheetTitle>
+        </SheetHeader>
+        <div>
+          {cartIsLoading && <p>Carregando...</p>}
+          {cart?.items.map((item) => (
+            <div key={item.id} className="mb-4 flex gap-4">
+              <div>
+                <Image
+                  src={item.productVariant.imageUrl}
+                  alt={item.productVariant.product.name}
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div>
+                <h3 className="font-medium">
+                  {item.productVariant.product.name}
+                </h3>
+                <p>Quantidade: {item.quantity}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </SheetContent>
     </Sheet>
   );
 };
